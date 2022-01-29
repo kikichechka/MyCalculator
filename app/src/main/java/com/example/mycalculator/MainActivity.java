d620553cfd0dd2503c20147a9cbf3bc876979417
kikichechka <kikichechka@mail.ru>
2022-01-29 10:50:23 +0300

    Added functionality and design.

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static boolean flag = true;
+    protected static TextView input;
     protected static TextView result;
+    protected static String symbol;
+    protected static float answer = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<String> list_number = new ArrayList<>();
-        //ArrayList<String> list_number2 = new ArrayList<>();
 
 
         // поле вывода текста
+        input = findViewById(R.id.input);
+
+        // поле вывода результата
         result = findViewById(R.id.result);
 
         // кнопка "с" удаление всех значений из коллекций
@@ -31,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 list_number.clear();
-                result.setText("0");
+                input.setText("0");
+                answer = 0.0F;
+                symbol = null;
+                result.setText("" + answer);
             }
         });
 
@@ -39,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_multiplication).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "*");
+                methodAddSymbol(list_number, "*");
             }
         });
 
@@ -47,15 +54,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_division).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, ":");
-            }
-        });
-
-        // кнопка удаление последнего элемента
-        findViewById(R.id.button_division).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                delete_1(list_number);
+                methodAddSymbol(list_number, "÷");
             }
         });
 
@@ -63,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_minus).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "-");
+                methodAddSymbol(list_number, "-");
             }
         });
 
@@ -71,15 +70,22 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_plus).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "+");
+                methodAddSymbol(list_number, "+");
             }
         });
 
         // кнопка равно
-        findViewById(R.id.button_division).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button_equals).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                symbol = null;
+                list_number.clear();
+                calculation(list_number,String.valueOf(answer));
+                //list_number.add(0, );
+                //view(list_number);
 
+                answer = 0.0f;
+                result.setText("" + answer);
             }
         });
 
@@ -87,24 +93,23 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_percent).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "%");
+                methodAddSymbol(list_number, "%");
             }
         });
 
-        // кнопка запятая
-        findViewById(R.id.button_comma).setOnClickListener(new View.OnClickListener() {
+        // кнопка точка
+        findViewById(R.id.button_dot).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, ",");
+                calculation(list_number, ".");
             }
         });
-
 
         // 1
         findViewById(R.id.button_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "1");
+                calculation(list_number, "1");
             }
         });
 
@@ -112,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "2");
+                calculation(list_number, "2");
             }
         });
 
@@ -120,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "3");
+                calculation(list_number, "3");
             }
         });
 
@@ -128,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "4");
+                calculation(list_number, "4");
             }
         });
 
@@ -136,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_5).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "5");
+                calculation(list_number, "5");
             }
         });
 
@@ -144,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_6).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "6");
+                calculation(list_number, "6");
             }
         });
 
@@ -152,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_7).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "7");
+                calculation(list_number, "7");
             }
         });
 
@@ -160,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_8).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "8");
+                calculation(list_number, "8");
             }
         });
 
@@ -168,7 +173,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_9).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "9");
+                calculation(list_number, "9");
             }
         });
 
@@ -176,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                method(list_number, "0");
+                calculation(list_number, "0");
             }
         });
     }
@@ -185,26 +190,92 @@ public class MainActivity extends AppCompatActivity {
     /**
      * метод добавления в коллекцию чисел
      */
-    public static void method(ArrayList list, String value) {
-        list.add(value);
-
-        String value_result = "";
-        for (Object o : list) {
-            value_result += o;
+    public static void calculation(ArrayList list, String value) {
+        if (symbol == null) { // если символ арифметической операции не был введен ранее
+            list.add(value); // добавляем цифру в коллекцию
+            view(list); // выводим коллекцию в textView
         }
-
-        //list.toString().replace("[", "").replace("]", "").replace(",", "");
-
-        result.setText(value_result);
+        else { // если символ арифметической операции был введен ранее
+            list.add(value); // добавляем цифру в коллекцию
+            flag = true;
+            arithmeticOperation(list); // выполняем арифметическую операцию с двумя числами, выведенными из массива
+            view(list); // выводим коллекцию в textView
+            result.setText("" + answer); // выводим результат в textViewResult
+        }
     }
 
-    public static void delete_1(ArrayList list) {
-        list.remove(list.size()-2);
+    /**
+     * метод добавления в коллекцию символов
+     */
+    public static void methodAddSymbol(ArrayList list, String value) {
+        if (list.size() != 0 && flag == true) {
+            flag = false;
+            symbol = value;
+            list.add(value);
+            view(list);
+        }
+    }
 
+    /**
+     * Метод вывода значений Arraylist в textView
+     *
+     * @param list
+     */
+    public static void view(ArrayList list) {
         String value_result = "";
         for (Object o : list) {
             value_result += o;
         }
-        result.setText(value_result);
+        input.setText(value_result);
+    }
+
+    /**
+     * // вычисление двух чисел, взятых из коллекции
+     */
+    public static void arithmeticOperation(ArrayList list) {
+        int symbolNumber = list.lastIndexOf(symbol); // записали индекс последнего символа в коллекции
+        String numberOne = "";
+        String numberTwo = "";
+        float number1 = 0f;
+        float number2 = 0f;
+
+        for (int i = 0; i < symbolNumber; i++) {
+            if (list.get(i).equals("÷") || list.get(i).equals("*") || list.get(i).equals("-") // если до текущего ввода символа арифметической операции был введен символ
+                    || list.get(i).equals("+") || list.get(i).equals("%")) {
+                number1 = answer; // присваиваем номеру 1 значение ответа
+                break;
+            }
+        }
+
+        if (number1 < 0.1) { //  если до текущего ввода символа арифметической операции не был введен другой символ
+            for (int i = 0; i < symbolNumber; i++) {
+                numberOne += list.get(i);
+            }
+            number1 = Float.valueOf(numberOne); // извлекли из массива первое число
+        }
+
+        for (int i = symbolNumber + 1; i < list.size(); i++) {
+            numberTwo += list.get(i);
+        }
+        number2 = Float.valueOf(numberTwo); // извлекли из массива второе число
+
+
+        switch (symbol) {
+            case "÷":
+                answer = number1 / number2;
+                break;
+            case "*":
+                answer = number1 * number2;
+                break;
+            case "-":
+                answer = number1 - number2;
+                break;
+            case "+":
+                answer = number1 + number2;
+                break;
+            case "%":
+                answer = number1 * number2 / 100;
+                break;
+        }
     }
 }
